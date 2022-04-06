bb69831d8ccab908b2afade74510ab3c644bd549
Karthikeyan RA <82165590+karthikeyanra@users.noreply.github.com>
2022-04-06 09:57:12 +0530

    Update list_running_instance_all_region.py

--- list_running_instance_all_region.py
+++ list_running_instance_all_region.py
@@ -2,6 +2,7 @@ import boto3
 
 ec2 = boto3.client('ec2')
 response = ec2.describe_regions()
+result=""
 for regions in response['Regions']:
     client=boto3.client('ec2',region_name=regions['RegionName'])
     #get all instance details for a region
@@ -9,7 +10,6 @@ for regions in response['Regions']:
     for reservation_details in instance_details['Reservations']:
         for instance in reservation_details['Instances']:
             if instance['State']['Name']=="running":
-                print("-------{}---------".format(regions['RegionName']))
-                print(instance['InstanceId'],instance['State']['Name'])
-
-    
+                result=result+"-------{}---------".format(regions['RegionName'])+"\n"
+                result=result+"{},{}".format(instance['InstanceId'],instance['State']['Name'])+"\n"
+print(result)
